d70316247bf6b40a9968419df9226e6516a129f3
SnowFox <65253170+ISnowFoxI@users.noreply.github.com>
2021-08-18 01:58:42 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const hamburgerButton = document.getElementById("hamburgerButton")
+const menu  = document.getElementById("menu");
+
+hamburgerButton.addEventListener("click", (e)=> {
+    if (e.target.className.includes("hamburgerButtonClicked")) { 
+        hamburgerButton.classList.remove("hamburgerButtonClicked") 
+    }
+    
+    else{
+    hamburgerButton.classList.add("hamburgerButtonClicked")
+    }
+
+    if(menu.className.includes("menusIsOpen")){ 
+        menu.classList.remove("menusIsOpen")
+    }
+    else { 
+        menu.classList.add("menusIsOpen")
+    }
+}
+)
